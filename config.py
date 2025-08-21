04b8b96d0dafb3169db3c05943991ca91f9b4d5c
Orangeliquid <hardym2020111@gmail.com>
2025-08-21 00:26:22 -0400

    Add config.py for project constants used throughout

---
+++ config.py
@@ -0,0 +1,42 @@
+TEST_OPTIONS = {
+    5: {
+        "option_name": "Fast Five",
+        "option_color": "#FF10F0",
+    },
+    10: {
+        "option_name": "Power Ten",
+        "option_color": "#10FFB1",
+    },
+    15: {
+        "option_name": "Funky Fifteen",
+        "option_color": "#FF4500",
+    },
+    20: {
+        "option_name": "Plenty Twenty",
+        "option_color": "#1E90FF",
+    },
+    30: {
+        "option_name": "Sturdy Thirty",
+        "option_color": "#228B22",
+    },
+    50: {
+        "option_name": "Nifty Fifty",
+        "option_color": "#8A2BE2",
+    },
+}
+
+INIT_STATS = {
+        "total_questions": 0,
+        "total_correct": 0,
+    }
+
+EXAM_OVERVIEW = {
+    "exam_format": "The CCRP exam consists of 130 multiple-choice questions.",
+    "scoring": "Only 100 questions are scored; the remaining 30 are unscored and used for statistical analysis.",
+    "passing_score": "Candidates need to correctly answer at least 72 of the 100 scored questions to pass.",
+    "guessing": "Candidates are encouraged to attempt all questions, as there is no penalty for wrong answers.",
+    "content": "The exam covers various topics in clinical research, including clinical trial design and management, "
+               "regulatory compliance, ethical issues, and data management.",
+    "graders": "The passing score is determined by a panel of experts using the Modified Angoff Method according to "
+               "SOCRA.",
+}
